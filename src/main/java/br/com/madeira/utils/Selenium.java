7077a117d93b979532bf5ca3ed1e57079bb7208f
Rafael Tulio <rafaeltulio9@gmail.com>
2017-01-17 19:20:31 -0200

    chrome

--- src/main/java/br/com/madeira/utils/Selenium.java
+++ src/main/java/br/com/madeira/utils/Selenium.java
@@ -4,9 +4,14 @@
 package br.com.madeira.utils;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.google.common.collect.ImmutableMap;
 
 /**
  * Classe Utils para o Selenium Identifica qual o browser escolhido no arquivo
@@ -24,19 +29,28 @@ public class Selenium {
 	 * driver apropriado e o retorna
 	 * 
 	 * @return retorna inst�ncia do WebDriver
+	 * @throws IOException 
 	 */
 
-	public static WebDriver getDriver() {
+	public static WebDriver getDriver() throws IOException {
 		String browser = Property.BROWSER_NAME;
 		if (driver == null) {
 			if (Browser.CHROME.equals(browser)) {
-				File file = new File(Property.CHROME_DRIVE_PATH);
-				System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+				ChromeDriverService service = new ChromeDriverService.Builder().usingDriverExecutable(new File(System.getProperty("webdriver.chrome.driver"))).usingAnyFreePort()
+					    .withEnvironment(ImmutableMap.of("DISPLAY", System.getProperty("webdriver.display"))).build();
+					  service.start();
+					  driver = new ChromeDriver(service, obterCapabilitiesChrome());				
+				
 				driver = new ChromeDriver();
 			}
 
 		}
 		return driver;
+	}
+
+	private static ChromeOptions obterCapabilitiesChrome() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }

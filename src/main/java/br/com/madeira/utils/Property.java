544616e8162863d2fed9af9a945ecb4b9c5cdec5
Rafael Tulio <rafaeltulio9@gmail.com>
2017-01-17 10:40:52 -0200

    chrome

--- src/main/java/br/com/madeira/utils/Property.java
+++ src/main/java/br/com/madeira/utils/Property.java
@@ -29,7 +29,7 @@ public class Property {
 
 	static {
 		
-		System.out.println(CHROME_DRIVE_PATH = new File("").getAbsolutePath() + "/src/main/resources/chromedriver");
+		System.out.println(CHROME_DRIVE_PATH = new File("").getAbsolutePath() + "/root/node_modules/chromedriver/lib/chromedriver/chromedriver");
 		IE64_DRIVE_PATH = new File("").getAbsolutePath() + "/src/main/resources/IEDriverServer.exe";
 		FIREFOX_DRIVE_PATH = new File("").getAbsolutePath() + "%PROGRAMFILES%\\Mozilla Firefox\\firefox.exe";
 		BROWSER_NAME = get("browser.name");

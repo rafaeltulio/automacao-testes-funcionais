6315431d697c5a8f1ee7a80cee345f93bd9dcc14
Rafael Tulio <rafaeltulio9@gmail.com>
2017-01-18 14:34:34 -0200

    chrome

--- src/main/java/br/com/madeira/utils/Browser.java
+++ src/main/java/br/com/madeira/utils/Browser.java
@@ -9,6 +9,6 @@ public class Browser {
 	public static final String FIREFOX = "firefox";
 	public static final String CHROME = "chrome";
 	public static final String IE = "ie";
-	public static final String SAFARI = "safari";
+	// public static final String SAFARI = "safari";
 
 }
